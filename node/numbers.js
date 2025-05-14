3db761efbf9689342206960f3901b890944a7b74
srikanthpragada <srikanthpragada@gmail.com>
2025-05-14 18:58:06 +0530

    14-MAY

---
+++ node/numbers.js
@@ -0,0 +1,11 @@
+function isEven(n) {
+    return n % 2 == 0 
+}
+
+function isOdd(n) {
+    return n % 2 == 0 
+}
+
+exports.isEven = isEven 
+exports.isOdd = isOdd 
+
---
+++ node/use_numbers.js
@@ -0,0 +1,5 @@
+// Import numbers.js 
+var nm = require("./numbers")
+
+console.log(nm)
+console.log(nm.isEven(10))
